4957d9b9e253c8537c851f605fcad1cc8d3960be
theoboraud <113908943+VirgileBoraud@users.noreply.github.com>
2024-06-24 12:56:26 +0200

    Creation of Llamag class and test of different implementation of llama3 and prompt.
    I tested different way of implementing llama3 with RAG.
    
    Isssues are still notable when we ask for a code. The class don't recognise it and search through the RAG instead of creating a code => similarity_threshold passed to 75 to solve the issue temporally.

---
+++ split.py
@@ -0,0 +1,21 @@
+# Load the Q&A data
+with open('Q&A_format.md', 'r', encoding='utf-8') as file:
+    data = file.read()
+
+# Manually split the document based on headers
+questions_answers = data.split("Question: ")
+
+# Process the split data into a structured format
+qa_pairs = []
+for qa in questions_answers[1:]:  # Skipping the first empty split
+    parts = qa.split("Answer: ")
+    question = parts[0].strip()
+    answer = parts[1].strip() if len(parts) > 1 else ""
+    qa_pairs.append({'question': question, 'answer': answer})
+
+print(qa_pairs)
+
+# Write the structured data to a new file
+with open('spliting_format.md', 'a', encoding='utf-8') as file:
+    for pair in qa_pairs:
+        file.write(f"{{question: \"{pair['question']}\", answer: \"{pair['answer']}\"}}\n")
