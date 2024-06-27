144d0b05c342d4cb30b1b6df97afcc8fc4236c4f
theoboraud <113908943+VirgileBoraud@users.noreply.github.com>
2024-06-27 09:55:16 +0200

    Implementation of new scrapping method and new document

--- test_code_given_by_llamag.py
+++ test_code_given_by_llamag.py
@@ -1,25 +1,22 @@
-import numpy as np
 from reservoirpy.nodes import Reservoir, Ridge
+import numpy as np
 
-# Parameters
-n_inputs = 10
-n_hidden = 100
-n_outputs = 1
-leak_rate = 0.2
-spectral_radius = 0.9
-learning_rate = 0.01
+# Set up the Mackey-Glass node
+node = Ridge(delta=0.1, alpha=0.2, beta=0.3, tau_min=5, tau_max=15)
 
-# Create the reservoir
-reservoir = Reservoir(n_inputs=n_inputs, n_units=n_hidden, leak_rate=leak_rate,
-                      spectral_radius=spectral_radius)
+# Generate some input data (e.g., a Mackey-Glass sequence)
+x = [0.9]
+for i in range(1000):
+    x.append(0.4 * x[-1] + 0.3 * x[-2] - 0.1 * x[-3] + 0.6)
 
-# Create the readout
-readout = Ridge(n_in=n_hidden, n_out=n_outputs, learning_rate=learning_rate)
+# Run the input through the reservoir
+y = []
+for t, xi in enumerate(x):
+    y.append(node(xi))
 
-# Train the network
-X_train = np.random.rand(1000, n_inputs)
-y_train = np.random.rand(1000, n_outputs)
-reservoir.fit(X_train, y_train, epochs=100)
-y_pred = reservoir.run(X_train)
-
-print("Root Mean Squared Error:", np.mean((y_pred - y_train) ** 2))
+# Visualize the output
+import matplotlib.pyplot as plt
+plt.plot(y)
+plt.xlabel('Time')
+plt.ylabel('Output')
+plt.show()
