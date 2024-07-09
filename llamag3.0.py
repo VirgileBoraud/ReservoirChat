de9ba672179ef5fd1867a7bb0c8a439375f68bca
Virgile Boraud <virgile.boraud@esme.fr>
2024-07-09 11:55:56 +0200

    Fetching issues fixed

--- llamag3.0.py
+++ llamag3.0.py
@@ -45,6 +45,76 @@ class LLaMag:
             print(f"Error getting embeddings: {e}")
             return None
     
+    def file_list(self, directory_path):
+        try:
+            # Get the list of files in the directory
+            files = os.listdir(directory_path)
+            
+            # Filter out directories, only keep files and add the directory path to each file name
+            file_names = [os.path.join(directory_path, file) for file in files if os.path.isfile(os.path.join(directory_path, file))]
+            
+            return file_names
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return []
+    
+    def load_data(self, filepaths):
+        try:
+            with ThreadPoolExecutor() as executor:
+                results = executor.map(self.process_file, filepaths)
+
+            all_qa_pairs = [qa for result in results for qa in result]
+
+            if not all_qa_pairs:
+                print("No data loaded.")
+                return
+
+            self.df = pd.DataFrame(all_qa_pairs)
+            self.df['ticket_embedding'] = self.df['ticket'].apply(lambda x: self.get_embedding(x))
+            self.df.to_csv('embeddings.csv', index=False)
+            print(f"Data saved to embeddings.csv")
+        except Exception as e:
+            print(f"Error loading data: {e}")
+
+    def process_file(self, filepath):
+        all_entries = []
+        document_name = os.path.basename(filepath)
+        try:
+            with open(filepath, 'r', encoding='utf-8') as file:
+                data = file.read()
+
+            if "Q&A" in filepath:
+                # Process as Q&A document
+                questions_answers = data.split("Question: ")
+                for qa in questions_answers[1:]:
+                    parts = qa.split("Answer: ")
+                    question = parts[0].strip()
+                    answer = parts[1].strip() if len(parts) > 1 else ""
+                    all_entries.append({"document": document_name,"ticket": question, "response": answer})
+
+            elif "codes" in filepath:
+                # Process as code document with titles
+                sections = data.split("## ")
+                for section in sections[1:]:
+                    lines = section.split('\n', 1)  # Split only on the first newline
+                    title = lines[0].strip()
+                    if len(lines) > 1:
+                        code_block_match = re.search(r'```python(.*?)```', lines[1].strip(), re.DOTALL)
+                        if code_block_match:
+                            code = f"```python{code_block_match.group(1).strip()}```"
+                            all_entries.append({"document": document_name,"ticket": title, "response": code})
+
+            else:
+                # Process as regular Markdown document without separating code
+                sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', data) ## I have to change things here
+                for sentence in sentences:
+                    if sentence.strip():
+                        all_entries.append({"document": document_name,"ticket": sentence.strip(), "response": sentence.strip()}) ## Use knowledge graph ot create big fan fiction of the docmet 9different concept, family, character, gemerate code to better respond, use this auestion to respond, split document, etc...)
+
+        except Exception as e:
+            print(f"Error processing file {filepath}: {e}")
+        return all_entries
+
     def cosine_similarity(self, a, b):
         return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
@@ -55,15 +125,18 @@ class LLaMag:
         self.df.dropna(subset=['ticket_embedding'], inplace=True)
         self.df['similarity'] = self.df['ticket_embedding'].apply(lambda x: self.cosine_similarity(query_embedding, x))
         top_n_indices = self.df[self.df['similarity'] >= self.similarity_threshold].nlargest(n, 'similarity').index
+        print(self.df.loc[top_n_indices])
         return self.df.loc[top_n_indices]
 
     def get_response(self, user_message):
         top_n_results = self.get_top_n_closest_texts(user_message)
-        top_n_texts = top_n_results['ticket'].tolist()
+        top_n_texts = ("document name: " + top_n_results['document'] + " | ticket: " + top_n_results['ticket'] + " | response: " + top_n_results['response']).tolist()
+        print(top_n_texts)
         system_message = self.message
         if "code" in user_message.lower():
             system_message += f"\n\nAdditional Code Content:\n{self.code_content}\n"
-        system_message += "\n".join([f"-> Text {i+1}: {t}" for i, t in enumerate(top_n_texts)])
+        system_message += "\n" + str(top_n_texts)
+        print(system_message)
         history = [
             {"role": "system", "content": system_message},
             {"role": "user", "content": user_message},
@@ -87,19 +160,22 @@ class LLaMag:
 system_message = '''You are Llamag, a helpful, smart, kind, and efficient AI assistant. 
         You are specialized in reservoir computing.
         You will serve as an interface to a RAG (Retrieval-Augmented Generation).
-        When given documents, you will respond using only these documents.
+        When given documents, you will respond using only these documents. They will serve as inspiration to your reponse.
         If you are not given any document, you will respond that you don't have the response in the database.
         When asked to code, you will use the given additional code content,
-        these documents are the only inspiration you can have on the reservoirPy library, you will never use the data you had previously acquired.
+        these documents are the only inspiration you can have on the reservoirPy library.
+        You will not return the exact responses your were provided, but they will serve as inspiration for your real response.
         You will never use the database you have acquired elsewhere other than in the documents given.
-        You will use the following documents to respond to your task.
+        You will however access the history of the conversation.
+        You will use the following documents to respond to your task (You will never display or talk about their existence, only use their information):
         DOCUMENTS:
+        (document name : where the name of the document with the similarity are, you will display the name of these documents when you are asked to give the source | ticket: Where the similar questions or embeddings are | answer: the answer or resulting embeddings that will serve you as inspiration to your answer)
         '''
 llamag = LLaMag(base_url="http://localhost:1234/v1", api_key="lm-studio", message=system_message, similarity_threshold=0.75, top_n=5)
 
 # Initialize conversation and history
 conversation = {
-    "Conversation": ["Present Yourself", "Hi! I am LLaMag. The Grrrreat RAG Fetcher, the reservoirPy know-how, the best rider of the High LlaMa tribe, How can I help you today?"]
+    "Conversation": ["Present Yourself", "Hi! I am LLaMag. The Grrrreat RAG Fetcher, the reservoirPy know-how, the best rider of the High LLaMa tribe, How can I help you today?"]
 }
 current_user_message = ""
 history = [
@@ -133,16 +209,17 @@ def style_conv(state: State, idx: int, row: int) -> str:
 page = """
 <|layout|columns=300px 1|
 <|part|class_name=sidebar|
-# LLaMag **Chat**{: .color-primary} # {: .logo-text}
+# **LL**{: .color-primary}a**M**{: .color-primary}ag # {: .logo-text}
 |>
 
 <|part|class_name=p2 align-item-bottom table|
 <|{conversation}|table|style=style_conv|show_all|>
-<|{current_user_message}|input|label=Write your message here...|on_action=send_message|class_name=fullwidth|>
+<|{current_user_message}|input|label=Write your message here...|on_action=send_message|class_name=fullwidth|change_delay=-1|>
 |>
 |>
 """
 
 # Run the Taipy GUI
 if __name__ == "__main__":
+    # llamag.load_data(llamag.file_list('doc/md'))
     Gui(page).run(debug=True, dark_mode=True, use_reloader=True, title="LLaMag")
